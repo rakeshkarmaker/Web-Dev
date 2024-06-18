25855268cbc820e4f7b5441c009aa2bbc0ff79eb
[rakeshkarmaker] <[rakeshkarmaker65@gmail.com]>
2024-06-18 21:25:52 +0600

    first commit

---
+++ LibraryManagement/action.php
@@ -0,0 +1,19 @@
+
+<?php
+if(isset($_POST['submit'])){
+	$_studentName = $_POST["Sname"]
+	$_studentID = $_POST["Sid"]
+	$_borrowedBook = $_POST["Bname"]
+	$_borrowedDate = $_POST["borrowDate"
+	echo "====================================================================<br><br>";
+	echo $_studentName;
+	echo "<br><br>";
+	echo $_studentID;
+	echo "<br><br>";
+	echo $_borrowedBook;
+	echo "<br><br>";
+	echo $_borrowedDate;
+	
+	}
+
+?>
